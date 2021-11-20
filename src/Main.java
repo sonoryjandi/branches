efb1866bd16d7c484e7a9d1f11ed4316f208d3bc
Nana <realyeolly@gmail.com>
2021-11-21 00:25:38 +0500

    test commit мы в пиве 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("hello, it's feature");
         System.out.println("hello");
         System.out.println("Мы в пиве");
+        System.out.println("тест пуш коммит в фиче(чекаут в мейн)");
     }
 }
